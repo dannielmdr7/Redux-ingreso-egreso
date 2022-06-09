1ec7be536e71f492eb61b0f969eff3f7703a03c5
DanielMontenegro <60446735+dannielmdr7@users.noreply.github.com>
2022-06-09 13:40:46 -0500

    feature: base lista

---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,20 @@
+import { EstadisticaComponent } from './../ingreso-egreso/estadistica/estadistica.component';
+import { Routes } from '@angular/router';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+
+
+export const dashBoardRoutes : Routes = [
+  {
+    path:'',
+    component:EstadisticaComponent
+  },
+  {
+    path:'ingreso-egreso',
+    component:IngresoEgresoComponent
+  },
+  {
+    path:'detalle',
+    component:DetalleComponent
+  }
+]
